06d8e2536eaf2a47c16e8c92266b6946ca0f2efd
Julian Lastra <Julian.Lastra@gds.ey.com>
2021-07-13 18:51:12 -0300

    Add project files.

---
+++ lastra-julian_POO_Parcial2/Consigna.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lastra_julian_POO_Parcial2
+{
+    class Consigna
+    {/*
+        Tema 11
+                    Sistema de seguimiento de envíos
+
+        El sistema de seguimiento de envíos se va a ocupar de recepcionar algún tipo de paquete el cual 
+        será remitido a algún tipo de cliente desde algún tipo de proveedor.
+        El sistema de seguimiento de envíos deberá constar de un registro de clientes, de proveedores, 
+        registro de móviles que se van a ocupar
+        de la entrega y un cierto orden de entrega dependiendo de la fecha en que se recepciona el paquete.
+        Los paquetes deberán ser entregados en el término de 72 horas por lo cual, en el momento de registrar 
+        el envío, automáticamente el sistema debe calcular la fecha de llegada del paquete a destino.
+        El sistema de registración de envío de seguimiento va a contar con los nombres de los responsables, 
+        tanto del registro del pedido como
+        así también del registro de la persona que traslada el paquete y de la persona que recepciona el envío.
+        Por cada envío será registrado con el móvil la fecha, lugar de salida, lugar de destino, y 
+        el costo que ha de tener ese envío.
+        El sistema permitirá ver por cada Destinatario los envíos realizados y por cada envío los detalles del mismo. 
+        Cada vez que se confirma la recepción del paquete el sistema dará por cerrado el proceso en cuestión.
+        Si existe demora el sistema deberá registrar las causas.
+        Puede persistir datos en archivos CSV. 
+        */
+    }
+}
